5d3926dd6a4dd9425fb85c1f2bfbd851d8762692
James Kizer <jdkizer9@users.noreply.github.com>
2021-07-21 13:26:54 -0600

    Added validation script

---
+++ scripts/test/common_test.py
@@ -0,0 +1,499 @@
+import os
+import unittest
+import json
+from common import (
+    read_issuer_entries_from_tsv_file, IssuerEntry, validate_entries, ValidationResult,
+    validate_key, IssueType, validate_keyset, Issue
+)
+
+FIXTURE_DIRECTORY = f'{os.path.dirname(__file__)}/fixtures'
+
+class ReadIssuerEntriesFromTSVFileTestCase(unittest.TestCase):
+
+    def test1(self):
+        actual = read_issuer_entries_from_tsv_file(
+            f'{FIXTURE_DIRECTORY}/example1.tsv',
+            encoding='ISO-8859-1'
+        )
+
+        expected = [
+            IssuerEntry('State of California', 'https://myvaccinerecord.cdph.ca.gov/creds'),
+            IssuerEntry('State of Louisiana', 'https://healthcardcert.lawallet.com'),
+        ]
+
+        self.assertEqual(actual, expected)
+
+    def test2(self):
+        actual = read_issuer_entries_from_tsv_file(
+            f'{FIXTURE_DIRECTORY}/example2.tsv',
+            encoding='ISO-8859-1'
+        )
+
+        expected = [
+            IssuerEntry('Example Issuer', 'https://example.com/issuer'),
+        ]
+
+        self.assertEqual(actual, expected)
+
+    def test_duplicate_issuer_names(self):
+        actual = read_issuer_entries_from_tsv_file(
+            f'{FIXTURE_DIRECTORY}/duplicate_issuer_names.tsv',
+            encoding='ISO-8859-1'
+        )
+
+        expected = [
+            IssuerEntry('Example Issuer', 'https://example.com/issuer'),
+            IssuerEntry('Example Issuer', 'https://example.com/issuer2'),
+        ]
+
+        self.assertEqual(actual, expected)
+
+    def test_duplicate_iss_values(self):
+        actual = read_issuer_entries_from_tsv_file(
+            f'{FIXTURE_DIRECTORY}/duplicate_iss_values.tsv',
+            encoding='ISO-8859-1'
+        )
+
+        ## ensure that last value seen in file is preserved
+        expected = [
+            IssuerEntry('Example Issuer 2', 'https://example.com/issuer'),
+        ]
+
+        self.assertEqual(actual, expected)
+
+    def test_no_entries(self):
+        actual = read_issuer_entries_from_tsv_file(
+            f'{FIXTURE_DIRECTORY}/no_entries.tsv',
+            encoding='ISO-8859-1'
+        )
+
+        expected = []
+
+        self.assertEqual(actual, expected)
+
+    def test_empty_file(self):
+        actual = read_issuer_entries_from_tsv_file(
+            f'{FIXTURE_DIRECTORY}/empty_file.tsv',
+            encoding='ISO-8859-1'
+        )
+
+        expected = []
+
+        self.assertEqual(actual, expected)
+
+class ValidateKeyTestCase(unittest.TestCase):
+
+    def test_valid_sig_key(self):
+        jwk_json = """
+        {
+            "kty": "EC",
+            "use": "sig",
+            "alg": "ES256",
+            "kid": "54M7LspsUfvbirxoLfGeTQp8oCVducfvt0DEU8W4Wcc",
+            "crv": "P-256",
+            "x": "SM85B9i8alfba9WcWehUYY5WTn6lnRQ9ivlOGrIELzY",
+            "y": "I9Agmt_PyqNv3LLkcCBA3iNmi9dieDNrXHnQdplNvHI"
+        }
+        """
+
+        jwk = json.loads(jwk_json)
+
+        (actual_is_valid, actual_issues) = validate_key(jwk)
+        actual_issue_types = [issue.type for issue in actual_issues]
+
+        expected_is_valid = True
+        expected_issue_types = []
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issue_types, expected_issue_types)
+
+    def test_malformed_key(self):
+        jwk_json = """
+        {
+            "kty": "EC",
+            "use": "sig",
+            "alg": "ES256",
+            "kid": "54M7LspsUfvbirxoLfGeTQp8oCVducfvt0DEU8W4Wcc",
+            "crv": "P-256",
+            "x": "SM85B9i8alfba9WcWehUYY5WTn6lnRQ9ivlOGrIELzY"
+        }
+        """
+
+        jwk = json.loads(jwk_json)
+
+        (actual_is_valid, actual_issues) = validate_key(jwk)
+        actual_issue_types = [issue.type for issue in actual_issues]
+
+        expected_is_valid = False
+        expected_issue_types = [
+            IssueType.KEY_IS_INVALID
+        ]
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issue_types, expected_issue_types)
+
+    def test_kid_missing(self):
+        jwk_json = """
+        {
+            "kty": "EC",
+            "use": "sig",
+            "alg": "ES256",
+            "crv": "P-256",
+            "x": "SM85B9i8alfba9WcWehUYY5WTn6lnRQ9ivlOGrIELzY",
+            "y": "I9Agmt_PyqNv3LLkcCBA3iNmi9dieDNrXHnQdplNvHI"
+        }
+        """
+
+        jwk = json.loads(jwk_json)
+
+        (actual_is_valid, actual_issues) = validate_key(jwk)
+        actual_issue_types = [issue.type for issue in actual_issues]
+
+        expected_is_valid = False
+        expected_issue_types = [
+            IssueType.KID_IS_MISSING
+        ]
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issue_types, expected_issue_types)
+
+    def test_invalid_kid(self):
+        jwk_json = """
+        {
+            "kty": "EC",
+            "use": "sig",
+            "alg": "ES256",
+            "kid": "54M7LspsUfvGeTQp8oCVducfvt0DEU8W4Wcc",
+            "crv": "P-256",
+            "x": "SM85B9i8alfba9WcWehUYY5WTn6lnRQ9ivlOGrIELzY",
+            "y": "I9Agmt_PyqNv3LLkcCBA3iNmi9dieDNrXHnQdplNvHI"
+        }
+        """
+
+        jwk = json.loads(jwk_json)
+
+        (actual_is_valid, actual_issues) = validate_key(jwk)
+        actual_issue_types = [issue.type for issue in actual_issues]
+
+        expected_is_valid = False
+        expected_issue_types = [
+            IssueType.KID_IS_INCORRECT
+        ]
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issue_types, expected_issue_types)
+
+    def test_valid_enc_key(self):
+        jwk_json = """
+        {
+            "kty": "EC",
+            "use": "enc",
+            "alg": "ECDH-ES",
+            "kid": "UoGD6QXSfg5glPtfg9sgKQzmUkUtCYb9Df2oidXXkeA",
+            "crv": "P-256",
+            "x": "ULq4jmu0kzCgJRSUuR2hvKGJfXZmX0ckGIRpYYdvbQw",
+            "y": "wNv2WCwH3if340DrtfpO9netZt_Cr9Po4FcYkNWFxf0"
+        }
+        """
+
+        jwk = json.loads(jwk_json)
+
+        (actual_is_valid, actual_issues) = validate_key(jwk)
+        actual_issue_types = [issue.type for issue in actual_issues]
+
+        expected_is_valid = False
+        expected_issue_types = [
+            IssueType.KEY_USE_IS_INCORRECT,
+            IssueType.KEY_ALG_IS_INCORRECT
+        ]
+
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issue_types, expected_issue_types)
+
+    def test_contains_private_key_material(self):
+        jwk_json = """
+        {
+            "kty": "EC",
+            "use": "sig",
+            "alg": "ES256",
+            "kid": "54M7LspsUfvbirxoLfGeTQp8oCVducfvt0DEU8W4Wcc",
+            "crv": "P-256",
+            "x": "SM85B9i8alfba9WcWehUYY5WTn6lnRQ9ivlOGrIELzY",
+            "y": "I9Agmt_PyqNv3LLkcCBA3iNmi9dieDNrXHnQdplNvHI",
+            "d": "kmCdd0MVkSDEWlhesvOMEkx99hSA5ZFcvpaqCvEUI9o"
+        }
+        """
+
+        jwk = json.loads(jwk_json)
+
+        (actual_is_valid, actual_issues) = validate_key(jwk)
+        actual_issue_types = [issue.type for issue in actual_issues]
+
+        expected_is_valid = False
+        expected_issue_types = [
+            IssueType.KEY_CONTAINS_PRIVATE_MATERIAL
+        ]
+
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issue_types, expected_issue_types)
+
+class ValidateKeysetTestCase(unittest.TestCase):
+
+    def test_keys_prop_missing(self):
+        jwks_json = """
+        {
+            "test": "value"
+        }
+        """
+
+        jwks = json.loads(jwks_json)
+        (actual_is_valid, actual_issues) = validate_keyset(jwks)
+        actual_issue_types = [issue.type for issue in actual_issues]
+
+        expected_is_valid = False
+        expected_issue_types = [
+            IssueType.KEYS_PROPERTY_MISSING
+        ]
+
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issue_types, expected_issue_types)
+
+    def test_empty_keys_prop(self):
+        jwks_json = """
+        {
+            "keys": []
+        }
+        """
+
+        jwks = json.loads(jwks_json)
+        (actual_is_valid, actual_issues) = validate_keyset(jwks)
+        actual_issue_types = [issue.type for issue in actual_issues]
+
+        expected_is_valid = False
+        expected_issue_types = [
+            IssueType.KEYS_PROPERTY_EMPTY
+        ]
+
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issue_types, expected_issue_types)
+
+    def test_valid_keyset_1_sig(self):
+
+        jwks_json = """
+        {
+            "keys": [
+                {
+                    "kty": "EC",
+                    "use": "sig",
+                    "alg": "ES256",
+                    "kid": "54M7LspsUfvbirxoLfGeTQp8oCVducfvt0DEU8W4Wcc",
+                    "crv": "P-256",
+                    "x": "SM85B9i8alfba9WcWehUYY5WTn6lnRQ9ivlOGrIELzY",
+                    "y": "I9Agmt_PyqNv3LLkcCBA3iNmi9dieDNrXHnQdplNvHI"
+                }
+            ]
+        }
+        """
+
+        jwks = json.loads(jwks_json)
+        (actual_is_valid, actual_issues) = validate_keyset(jwks)
+        actual_issue_types = [issue.type for issue in actual_issues]
+
+        expected_is_valid = True
+        expected_issue_types = []
+
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issue_types, expected_issue_types)
+
+    def test_valid_keyset_2_sig(self):
+
+        jwks_json = """
+        {
+            "keys": [
+                {
+                    "kty": "EC",
+                    "use": "sig",
+                    "alg": "ES256",
+                    "kid": "54M7LspsUfvbirxoLfGeTQp8oCVducfvt0DEU8W4Wcc",
+                    "crv": "P-256",
+                    "x": "SM85B9i8alfba9WcWehUYY5WTn6lnRQ9ivlOGrIELzY",
+                    "y": "I9Agmt_PyqNv3LLkcCBA3iNmi9dieDNrXHnQdplNvHI"
+                },
+                {
+                    "kty": "EC",
+                    "use": "sig",
+                    "alg": "ES256",
+                    "kid": "5iNYX3Im0lBD4B3tZTQRkDw1BsJROIrcnYOsb6qjAHM",
+                    "crv": "P-256",
+                    "x": "4Tisi9KVtl3YRZailW14pHCVGSBnkR8EXd1RUQ36egc",
+                    "y": "hx8z_yr_yVaor-lZsVPeGKC8RT1Vk4iX-gxjZKF8MK0"
+                }
+            ]
+        }
+        """
+
+        jwks = json.loads(jwks_json)
+        (actual_is_valid, actual_issues) = validate_keyset(jwks)
+        actual_issue_types = [issue.type for issue in actual_issues]
+
+        expected_is_valid = True
+        expected_issue_types = []
+
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issue_types, expected_issue_types)
+
+    def test_valid_keyset_1_sig_1_only(self):
+
+        jwks_json = """
+        {
+            "keys": [
+                {
+                    "kty": "EC",
+                    "use": "sig",
+                    "alg": "ES256",
+                    "kid": "54M7LspsUfvbirxoLfGeTQp8oCVducfvt0DEU8W4Wcc",
+                    "crv": "P-256",
+                    "x": "SM85B9i8alfba9WcWehUYY5WTn6lnRQ9ivlOGrIELzY",
+                    "y": "I9Agmt_PyqNv3LLkcCBA3iNmi9dieDNrXHnQdplNvHI"
+                },
+                {
+                    "kty": "EC",
+                    "use": "enc",
+                    "alg": "ECDH-ES",
+                    "kid": "UoGD6QXSfg5glPtfg9sgKQzmUkUtCYb9Df2oidXXkeA",
+                    "crv": "P-256",
+                    "x": "ULq4jmu0kzCgJRSUuR2hvKGJfXZmX0ckGIRpYYdvbQw",
+                    "y": "wNv2WCwH3if340DrtfpO9netZt_Cr9Po4FcYkNWFxf0"
+                }
+            ]
+        }
+        """
+
+        jwks = json.loads(jwks_json)
+        (actual_is_valid, actual_issues) = validate_keyset(jwks)
+
+        expected_is_valid = True
+        expected_issues = [
+            Issue('Key with kid=UoGD6QXSfg5glPtfg9sgKQzmUkUtCYb9Df2oidXXkeA has an incorrect key use. It should be \"sig\"', IssueType.KEY_USE_IS_INCORRECT),
+            Issue('Key with kid=UoGD6QXSfg5glPtfg9sgKQzmUkUtCYb9Df2oidXXkeA has an incorrect key alg. It should be \"ES256\"', IssueType.KEY_ALG_IS_INCORRECT),
+        ]
+
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issues, expected_issues)
+
+    def test_invalid_keyset_1_key_invalid(self):
+
+        jwks_json = """
+        {
+            "keys": [
+                {
+                    "kty": "EC",
+                    "use": "sig",
+                    "alg": "ES256",
+                    "kid": "54M7LspsUfvbirxoLfGeTQp8oCVducfvt0DEU8W4Wcc",
+                    "crv": "P-256",
+                    "x": "SM85B9i8alfba9WcWehUYY5WTn6lnRQ9ivlOGrIELzY",
+                    "y": "I9Agmt_PyqNv3LLkcCBA3iNmi9dieDNrXHnQdplNvHI"
+                },
+                {
+                    "kty": "EC",
+                    "use": "sig",
+                    "alg": "ES256",
+                    "kid": "5iNYX3Im0lBD4B3tZTQRkDw1BsJROIrcnYOsb6qjAHM",
+                    "crv": "P-256",
+                    "x": "4Tisi9KVtl3YRZailW14pHCVGSBnkR8EXd1RUQ36egc",
+                    "y": "hx8z_yr_yVaor-lZsVPeGKC8RT1Vk4iX-gxjZKF8MK0",
+                    "d": "0OaOfIFXiSLKfYVM01_4DjhleNafW4JjjMc50Ge0GRk"
+                }
+            ]
+        }
+        """
+
+        jwks = json.loads(jwks_json)
+        (actual_is_valid, actual_issues) = validate_keyset(jwks)
+
+        expected_is_valid = False
+        expected_issues = [
+            Issue('Key with kid=5iNYX3Im0lBD4B3tZTQRkDw1BsJROIrcnYOsb6qjAHM contains private key material', IssueType.KEY_CONTAINS_PRIVATE_MATERIAL),
+        ]
+
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issues, expected_issues)
+
+    def test_invalid_keyset_kid_missing(self):
+
+        jwks_json = """
+        {
+            "keys": [
+                {
+                    "kty": "EC",
+                    "use": "sig",
+                    "alg": "ES256",
+                    "kid": "54M7LspsUfvbirxoLfGeTQp8oCVducfvt0DEU8W4Wcc",
+                    "crv": "P-256",
+                    "x": "SM85B9i8alfba9WcWehUYY5WTn6lnRQ9ivlOGrIELzY",
+                    "y": "I9Agmt_PyqNv3LLkcCBA3iNmi9dieDNrXHnQdplNvHI"
+                },
+                {
+                    "kty": "EC",
+                    "use": "sig",
+                    "alg": "ES256",
+                    "crv": "P-256",
+                    "x": "4Tisi9KVtl3YRZailW14pHCVGSBnkR8EXd1RUQ36egc",
+                    "y": "hx8z_yr_yVaor-lZsVPeGKC8RT1Vk4iX-gxjZKF8MK0"
+                }
+            ]
+        }
+        """
+
+        jwks = json.loads(jwks_json)
+        (actual_is_valid, actual_issues) = validate_keyset(jwks)
+
+        expected_is_valid = False
+        expected_issues = [
+            Issue('kid is missing', IssueType.KID_IS_MISSING)
+        ]
+
+        self.assertEqual(actual_is_valid, expected_is_valid)
+        self.assertEqual(actual_issues, expected_issues)
+
+class ValidateEntriesTestCase(unittest.TestCase):
+
+    def test_validate_entries1(self):
+        entries = [
+            IssuerEntry('SHC Example Issuer 1', 'https://spec.smarthealth.cards/examples/issuer'),
+            IssuerEntry('SHC Example Issuer 2', 'https://spec.smarthealth.cards/examples/issuer'),
+            IssuerEntry('SHC Example Issuer 3', 'https://spec.smarthealth.cards/examples/issuer'),
+            IssuerEntry('SHC Example Issuer 4', 'https://spec.smarthealth.cards/examples/issuer'),
+            IssuerEntry('SHC Example Issuer 5', 'https://spec.smarthealth.cards/examples/issuer'),
+            IssuerEntry('SHC Example Issuer 6', 'https://spec.smarthealth.cards/examples/issuer'),
+            IssuerEntry('SHC Example Issuer 7', 'https://spec.smarthealth.cards/examples/issuer'),
+            IssuerEntry('SHC Example Issuer 8', 'https://spec.smarthealth.cards/examples/issuer'),
+            IssuerEntry('SHC Example Issuer 9', 'https://spec.smarthealth.cards/examples/issuer'),
+            IssuerEntry('SHC Example Issuer 10', 'https://spec.smarthealth.cards/examples/issuer')
+        ]
+
+        actual = validate_entries(entries)
+
+        expected = [ValidationResult(entry, True, []) for entry in entries]
+
+        self.assertEqual(actual, expected)
+
+    def test_valid_and_invalid_entry(self):
+        entries = [
+            IssuerEntry('SHC Example Issuer', 'https://spec.smarthealth.cards/examples/issuer'),
+            IssuerEntry('Invalid issuer 1', 'https://spec.smarthealth.cards/examples/iss'),
+            IssuerEntry('Invalid issuer 2', 'https://spec.smarthealth.cards/examples/issuer/'),
+        ]
+
+        actual = validate_entries(entries)
+        self.assertEqual(actual[0].issuer_entry, entries[0])
+        self.assertEqual(actual[0].is_valid, True)
+        self.assertEqual(actual[0].issues, [])
+
+        self.assertEqual(actual[1].issuer_entry, entries[1])
+        self.assertEqual(actual[1].is_valid, False)
+        self.assertEqual(actual[1].issues[0].type, IssueType.FETCH_EXCEPTION)
+
+        self.assertEqual(actual[2].issuer_entry, entries[2])
+        self.assertEqual(actual[2].is_valid, False)
+        self.assertEqual(actual[2].issues[0].type, IssueType.ISS_ENDS_WITH_TRAILING_SLASH)
+
+
+
---
+++ scripts/validate_entries.py
@@ -0,0 +1,25 @@
+import argparse
+import common
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Validates entries in a JSON file')
+    parser.add_argument('input_file', help='JSON file')
+    parser.add_argument('--show-warnings', action='store_true', help='print warnings to the console')
+
+    args = parser.parse_args()
+    entries_from_json = common.read_issuer_entries_from_json_file(args.input_file)
+
+    validation_results = common.validate_entries(entries_from_json)
+    valid = common.analyze_results(validation_results, args.show_warnings)
+
+    if valid:
+        print('All entries are valid')
+        exit(0)
+    else:
+        print('One or more entries are invalid')
+        exit(1)
+
+if __name__ == "__main__":
+    main()
