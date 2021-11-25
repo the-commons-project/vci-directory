2d2f58de7063eba0a7b8ae0bbe74d08e8a2460a3
James Kizer <jdkizer9@users.noreply.github.com>
2021-11-25 11:12:12 -0700

    Updated gh action to validate diffs

---
+++ scripts/validate_diffs.py
@@ -0,0 +1,34 @@
+import argparse
+import common
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Validates different entries')
+    parser.add_argument('head_input_file', help='JSON file')
+    parser.add_argument('base_input_file', help='JSON file')
+    parser.add_argument('--show-warnings', action='store_true', help='print warnings to the console')
+
+    args = parser.parse_args()
+    entries_from_json = common.read_issuer_entries_from_json_file(args.head_input_file)
+
+    ## ensure no duplicate iss values
+    duplicate_entries = common.duplicate_entries(entries_from_json)
+    if len(duplicate_entries) > 1:
+        print('Found duplicate entries')
+        for entry in duplicate_entries:
+            print(entry)
+        exit(1)
+
+    validation_results = common.validate_entries(entries_from_json)
+    valid = common.analyze_results(validation_results, True, args.show_warnings)
+
+    if valid:
+        print('All entries are valid')
+        exit(0)
+    else:
+        print('One or more entries are invalid')
+        exit(1)
+
+if __name__ == "__main__":
+    main()
